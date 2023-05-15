ee4379f3a72ab1b0f1c340320d8cab8f10d55bec
mahmudova.rahide <mahmudovarahide@gmail.com>
2023-05-15 15:09:46 +0400

    single page done

--- commerce-app/src/components/product-card/product-card.components.jsx
+++ commerce-app/src/components/product-card/product-card.components.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import Button from "../button/button.componnet";
 import { CardContext } from "../../context/card.context";
+import { NavLink } from "react-router-dom";
 
 const ProductCard = ({ product }) => {
   const { name, price, imageUrl } = product;
@@ -10,16 +11,18 @@ const ProductCard = ({ product }) => {
 
   return (
     <>
-      <div className="image-box">
-        <img className="w-100" src={imageUrl} alt={name} />
-      </div>
-      <div className="content-box">
-        <h6 className="product-name">{name}</h6>
-        <p className="product-price">{price}</p>
-        <Button buttonType="productButton" onClick={addProductToCart}>
-          Add to Cart
-        </Button>
-      </div>
+      <NavLink to={`/products/${product.id}`}>
+        <div className="image-box">
+          <img className="w-100" src={imageUrl} alt={name} />
+        </div>
+        <div className="content-box">
+          <h6 className="product-name">{name}</h6>
+          <p className="product-price">{price}</p>
+        </div>
+      </NavLink>
+      <Button buttonType="productButton" onClick={addProductToCart}>
+        Add to Cart
+      </Button>
     </>
   );
 };
--- commerce-app/src/pages/products/product-single/product-single.jsx
+++ commerce-app/src/pages/products/product-single/product-single.jsx
@@ -1,31 +1,43 @@
-import React, { useContext } from 'react';
-import { useParams } from 'react-router-dom';
-import { CategoriesContext } from '../../../context/categories.context';
+import React, { useContext } from "react";
+import { useParams } from "react-router-dom";
+import { CategoriesContext } from "../../../context/categories.context";
 
 const ProductSingle = () => {
   const { id } = useParams();
   const { categoriesMap } = useContext(CategoriesContext);
 
   let product = null;
-  Object.keys(categoriesMap).forEach(categoryKey => {
+  Object.keys(categoriesMap).forEach((categoryKey) => {
     const category = categoriesMap[categoryKey];
-    const foundProduct = category.find(product => product.id === id);
+    const foundProduct = category.find(
+      (product) => product.id === parseInt(id)
+    );
     if (foundProduct) {
       product = foundProduct;
     }
   });
 
   if (!product) {
-    return <div>Ürün bulunamadı</div>;
+    return <div className="container">
+      <h6>Ürün bulunamadı</h6>
+    </div>;
   }
 
-  const { name, imageUrl } = product;
+  const { name, imageUrl, price } = product;
 
   return (
-    <div>
-      <h2>{name}</h2>
-      <img src={imageUrl} alt={name} />
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6 image-box">
+          <img src={imageUrl} alt={name} className="w-75" />
+        </div>
+        <div className="col-md-6 content-box">
+          <h2>{name}</h2>
+          <p>{price}</p>
+        </div>
+      </div>
     </div>
   );
 };
-export default ProductSingle
+
+export default ProductSingle;
