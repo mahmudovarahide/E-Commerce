171e728e16597463ae86f42c0036cbd989d3e7a9
mahmudovarahide <mahmudovarahide@gmail.com>
2023-05-28 16:21:17 +0400

    reducers

--- commerce-app/src/context/user.context.jsx
+++ commerce-app/src/context/user.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useEffect, useReducer } from "react";
 import {
   createUserDocumentFromAuth,
   onAuthStateChangedListener,
@@ -9,8 +9,35 @@ export const UserContext = createContext({
   setCurrentUser: () => null,
 });
 
+const INITAL_STATE = {
+  currentUser: null,
+};
+
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: "SET_CURRENT_USER",
+};
+
+const userReducer = (state, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: payload,
+      };
+    default:
+      throw new Error(`Unhandled type ${type} in user Reducer`);
+  }
+};
+
 export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
+  
+  const [{ currentUser }, dispatch] = useReducer(userReducer, INITAL_STATE);
+
+  const setCurrentUser = (user) => {
+    dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
+  };
+
   const value = { currentUser, setCurrentUser };
   useEffect(() => {
     const unsubcribe = onAuthStateChangedListener((user) => {
